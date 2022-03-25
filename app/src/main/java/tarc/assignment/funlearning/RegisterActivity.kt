4e4cfe84af0c3a2ef90a04fb9943724c6b91a92c
victorwong02 <wongjw-wm20@student.tarc.edu.my>
2022-03-25 22:00:36 +0800

    login register UI

---
+++ app/src/main/java/tarc/assignment/funlearning/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package tarc.assignment.funlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
