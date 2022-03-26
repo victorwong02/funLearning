a3af4d8c2a8aaa84afacc77edec9a8b3cf50bc3e
victorwong02 <wongjw-wm20@student.tarc.edu.my>
2022-03-26 22:36:46 +0800

    apps keep stopping when running loginActivity. bug are not spotted yet, remove MainActivity.kt line 14 if you want to launch the app.

--- app/src/main/java/tarc/assignment/funlearning/MainActivity.kt
+++ app/src/main/java/tarc/assignment/funlearning/MainActivity.kt
@@ -3,12 +3,27 @@ package tarc.assignment.funlearning
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    checkUser()
 
+    }
+    private fun checkUser() {
+        // if user is logged in, go to main screen
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null)
+        {
+            //logged in
+            startActivity(Intent(this,LoginActivity::class.java))
+            TODO("Change this main activity to following activity")
+            finish()
+        }
     }
 }
