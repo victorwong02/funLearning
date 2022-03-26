21542b6c1b5384c0183d3093c6b5e3545b61a0bd
LaiSiHua <laish-wm20@student.tarc.edu.my>
2022-03-26 20:11:34 +0800

    added home,lesson fragments

---
+++ app/src/main/java/tarc/assignment/funlearning/HomeActivity.kt
@@ -0,0 +1,26 @@
+package tarc.assignment.funlearning
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeActivity.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+
+
+    }
+}
---
+++ app/src/main/java/tarc/assignment/funlearning/LessonHTMLChp1.kt
@@ -0,0 +1,23 @@
+package tarc.assignment.funlearning
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [lesson_html_chp1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class lesson_html_chp1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+
+
+    }
+}
