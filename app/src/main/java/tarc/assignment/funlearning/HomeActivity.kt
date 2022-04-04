e4b0c658f552ba8d5a07b89ecc6275d5233b4186
LaiSiHua <laish-wm20@student.tarc.edu.my>
2022-04-04 16:54:36 +0800

    edit home list

--- app/src/main/java/tarc/assignment/funlearning/HomeActivity.kt
+++ app/src/main/java/tarc/assignment/funlearning/HomeActivity.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * A simple [Fragment] subclass.
@@ -20,7 +22,18 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
 
+        val lang = findViewById<ListView>(R.id.type_of_language)
+        val langName = arrayOf("HTML", "C", "Java", "JavaScript", "Kotlin", "C++")
 
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1, langName
+        )
+
+        lang.adapter = arrayAdapter
+
+        lang.setOnItemClickListener { adapterView, view, i, l ->
+
+        }
 
     }
 }
