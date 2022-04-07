06481d60169018b68d40bea0950686d81a2df185
YauHuiYi <yauhy-wm20@student.tarc.edu.my>
2022-04-07 16:36:53 +0800

    profile

--- app/src/main/java/tarc/assignment/funlearning/Profile.kt
+++ app/src/main/java/tarc/assignment/funlearning/Profile.kt
@@ -6,24 +6,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import tarc.assignment.funlearning.databinding.FragmentProfileBinding
 
 class Profile : Fragment() {
 
-    private lateinit var binding: FragmentProfileBinding
+    private var _binding: FragmentProfileBinding? = null
+
+    private val binding get() = _binding!!
+
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//         binding.editProfile.setOnClickListener {
+
+//         }
+
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
-
 
         binding.logoutButton.setOnClickListener {
             firebaseAuth.signOut()
             checkUser()
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun checkUser() {
