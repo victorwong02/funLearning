5504fbb5308f6f91662c23995a7708e22c5f4624
LaiSiHua <laish-wm20@student.tarc.edu.my>
2022-04-08 23:22:59 +0800

    cardview, language(chapter)

--- app/src/main/java/tarc/assignment/funlearning/fragments/HomeFragment.kt
+++ app/src/main/java/tarc/assignment/funlearning/fragments/HomeFragment.kt
@@ -5,48 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import tarc.assignment.funlearning.R
 import tarc.assignment.funlearning.LangType
+import tarc.assignment.funlearning.LangTypeAdapter
 
 class HomeFragment : Fragment() {
+
+    private lateinit var adapter: LangTypeAdapter
+    val langList: List<LangType> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_home, container, false)
+    ): View? {
+        //inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.cardview_row, container, false)
 
+//        initRecyclerView(view)
+        return view
+    }
     // To do the functionality in this fragment, etc. make a toast
-
-    val langList = ArrayList<LangType>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initData()
-//        setRecyclerView()
 
     }
 
-//    private fun setRecyclerView() {
-//        val langAdapter = langTypeAdapter(langList)
-//        recyclerView.adapter = langAdapter
-//        recylcerView.setHasFixedSize(true)
+//    private fun initRecyclerView(view: View){
+//        val recyclerView = view.findViewById<RecyclerView>(R.id.list_language)
+//        recyclerView.layoutManager = LinearLayoutManager(activity)
+//        adapter = LangTypeAdapter(languageList, this)
+//        recyclerView.adapter =
+//
 //    }
 
-    private fun initData() {
-        langList.add(
-            LangType(
-            "Chapter 1 : Introduction to HTML",
-            "This chapter will show some of the tags that is important for HTML."
-        )
-        )
-        langList.add(
-            LangType(
-            "Chapter 2 : HTML Attributes",
-            "HTML attributes provide additional information about HTML elements."
-        )
-        )
-    }
 
 }
