6d7902a31772dd4f45a9f9c1688a68d665b3dd38
YauHuiYi <yauhy-wm20@student.tarc.edu.my>
2022-04-08 23:39:09 +0800

    profile to edit profile

--- app/src/main/java/tarc/assignment/funlearning/fragments/EditProfileFragment.kt
+++ app/src/main/java/tarc/assignment/funlearning/fragments/EditProfileFragment.kt
@@ -11,7 +11,6 @@ import com.google.firebase.auth.FirebaseAuth
 import tarc.assignment.funlearning.ForgetPasswordActivity
 import tarc.assignment.funlearning.R
 import tarc.assignment.funlearning.databinding.FragmentEditProfileBinding
-import tarc.assignment.funlearning.databinding.FragmentProfileBinding
 
 class EditProfileFragment : Fragment() {
 
@@ -20,40 +19,42 @@ class EditProfileFragment : Fragment() {
     private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
 
-    private var _binding1: FragmentProfileBinding? = null
-    private val binding1 get() = _binding1!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = binding.root
-
         _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
-        _binding1 = FragmentProfileBinding.inflate(inflater, container, false)
+
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.saveInfo.setOnClickListener{ editProfile() }
+        binding.saveInfo.setOnClickListener{
+            editProfile()
+
+            val fragmentManager =  parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val fragmentprofile = ProfileFragment()
+            fragmentTransaction.replace(R.id.nav_fragment, fragmentprofile)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+
+        }
 
         binding.changePswd.setOnClickListener{
             startActivity(Intent(context, ForgetPasswordActivity::class.java))
         }
 
+        val view = binding.root
         return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        _binding1 = null
     }
 
     private fun editProfile() {
+
         val newName = binding.editName.text.toString()
         val newEmail = binding.editEmail.text.toString()
 
@@ -62,18 +63,17 @@ class EditProfileFragment : Fragment() {
                 Toast.makeText(context, "Nothing Has Been Updated", Toast.LENGTH_SHORT).show()
             }
             else {
-                binding1.userEmail.text = getString(R.string.user_email,newEmail)
+
                 Toast.makeText(context, "Profile Updated", Toast.LENGTH_SHORT).show()
             }
         }
         else{
             if(newEmail.trim() == null) {
-                binding1.userName.text = getString(R.string.user_name,newName)
+
                 Toast.makeText(context, "Profile Updated", Toast.LENGTH_SHORT).show()
             }
             else {
-                binding1.userEmail.text = getString(R.string.user_email,newEmail)
-                binding1.userName.text = getString(R.string.user_name,newName)
+
                 Toast.makeText(context, "Profile Updated", Toast.LENGTH_SHORT).show()
             }
         }
