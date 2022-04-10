7de8f55adce0519969e9ef08f4d52d371ae209e2
YauHuiYi <yauhy-wm20@student.tarc.edu.my>
2022-04-10 22:25:27 +0800

    avatar

---
+++ app/src/main/java/tarc/assignment/funlearning/fragments/EditAvatarFragment.kt
@@ -0,0 +1,20 @@
+package tarc.assignment.funlearning.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import tarc.assignment.funlearning.R
+
+class EditAvatarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_avatar, container, false)
+    }
+
+}
